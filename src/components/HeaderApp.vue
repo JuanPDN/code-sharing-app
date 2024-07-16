bf55f2e0b9f4e603d984d38864379e3e0c34e763
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-07-16 17:58:34 -0500

    refactor(headerApp): separate component header of the main

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <img src="@/assets/NoteCodeLogo.svg" alt="NoteCode logo" class="logo">
+        <div>
+            <h1>Create & Share</h1>
+            <h2>Your Code easily</h2>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 12px;
+    margin: 36px 0px;
+}
+
+header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+h1 {
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 600;
+}
+
+h2 {
+    text-align: center;
+    font-size: 2.5rem;
+    font-weight: 600;
+}
+
+.logo {
+    margin-top: 40px;
+}
+</style>
