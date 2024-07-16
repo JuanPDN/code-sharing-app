79beb6cf753c49260d798bbce4f08b80495a1002
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-07-16 18:00:56 -0500

    refactor(app.vue): separated components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
-
+import MonacoEditor from '@/components/MonacoEditor.vue'
+import HeaderApp from '@/components/HeaderApp.vue'
 </script>
 
 <template>
   <div class="bg-hero">
-    "hello world"
+    <HeaderApp />
+    <MonacoEditor />
   </div>
 
 </template>
 
 <style scoped>
 .bg-hero {
-  height: 100vh;
-  background: url('./assets/Hero-Background-notecode.svg') no-repeat;
+  height: 100%;
+  background: url('@/assets/Hero-Background-notecode.svg') no-repeat;
   background-position: center top;
+  color: #121826;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 8px;
 }
 
 @media (min-width: 1280px) {
